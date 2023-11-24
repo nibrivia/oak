cb8bdd37dcda6778a716d16eac9f79bb0811d982
Olivia Brode-Roger <nibrivia@users.noreply.github.com>
2023-11-24 11:29:02 -0800

    refactor out session management

--- app.py
+++ app.py
@@ -1,57 +1,78 @@
 from flask import Flask, request, render_template, make_response
 import time
 import uuid
-from main import evalString, default_env
+from main import evalString, default_env, Env
 
 app = Flask(__name__)
 
 
-sessions = dict()
+class SessionRegistry:
+    def __init__(self):
+        self.sessions = dict()
+
+    def __getitem__(self, session_key: str) -> Env:
+        self._refresh_session(session_key)
+        (env, _) = self.sessions.get(session_key, (default_env, 0))
+
+        self.sessions[session_key] = (env, time.time())
+        self._clean_old_sessions()
+
+        return env
+
+    def _refresh_session(self, session_key: str) -> None:
+        if session_key in self.sessions:
+            (env, _) = self.sessions.get(session_key)
+            self.sessions[session_key] = (env, time.time())
+
+    def __setitem__(self, session_key: str, env: Env) -> None:
+        self.sessions[session_key] = (env, time.time())
+        self._clean_old_sessions()
+
+    def _clean_old_sessions(self) -> None:
+        keys_to_delete = []
+        for session_name, (_, date_created) in self.sessions.items():
+            if time.time() - date_created > 600:
+                keys_to_delete.append(session_name)
+
+        for k in keys_to_delete:
+            del self.sessions[k]
 
 
-def clean_old_sessions():
-    global sessions
-
-    keys_to_delete = []
-    for session_name, (_, date_created) in sessions.items():
-        print(f"Looking at {session_name}")
-        if time.time() - date_created > 600:
-            print(f"deleting {session_name}")
-            keys_to_delete.append(session_name)
-
-    for k in keys_to_delete:
-        del sessions[k]
-
-    return
+session_registry = SessionRegistry()
 
 
 @app.route("/")
-def testertester():
+def full_page_template():
     resp = make_response(render_template('index.html'))
 
-    session_name = uuid.uuid4().hex[:6].upper()
+    session_name = uuid.uuid4().hex
     resp.set_cookie('session', session_name)
-    sessions[session_name] = (default_env, time.time())
-    clean_old_sessions()
+    print(session_name)
+
+    session_registry[session_name] = default_env
 
     return resp
 
 
-@ app.route('/clicked')
-def clicked():
-    return "<p>Clicked</p>"
-
-
-@ app.route('/put', methods=['PUT'])
+@app.route('/put', methods=['PUT', 'OPTIONS'])
 def post():
+    if request.method == "OPTIONS":
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add('Access-Control-Allow-Headers', "*")
+        response.headers.add('Access-Control-Allow-Methods', "*")
+        return response
+
     session_name = request.cookies.get('session')
 
-    title = request.form.get('title')
-    (env, _) = sessions.get(session_name)
-    (res, new_env) = evalString(title, env)
+    env = session_registry[session_name]
+    command = request.form.get('title') or ""
+    (res, new_env) = evalString(command, env)
 
-    sessions[session_name] = (new_env, time.time())
-    clean_old_sessions()
+    session_registry[session_name] = new_env
 
-    return f"""<div class='p-2'><pre class='font-bold'>> {title}</pre>
-<pre>{res}</pre></div>"""
+    response = make_response(f"""<div class='p-2'><pre class='font-bold'>> {command}</pre>
+<pre>{res}</pre></div>""")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return response
