6ae71f8f7736bd985750db80295c9d02416d6bb1
Olivia Brode-Roger <nibrivia@users.noreply.github.com>
2023-11-30 14:33:27 -0500

    stuffs

--- app/Main.hs
+++ app/Main.hs
@@ -26,7 +26,8 @@ data Expression
 
 -- | Bindings only modify the environment
 data Binding
-  = Bind String Expression
+  = BindValue String Expression
+  | BindType String Expression
   deriving (Show, Eq)
 
 debugPipe :: (Show b) => String -> b -> b
@@ -34,28 +35,60 @@ debugPipe name x = traceShow (name ++ ": " ++ show x) x
 
 data Env
   = EmptyEnv
-  | Env (Map.Map String Expression) Env
+  | Env (Map.Map String Expression) (Map.Map String Expression) Env
   deriving (Show, Eq)
 
 envLookup :: Env -> String -> Expression
 envLookup EmptyEnv name = Error ("Value " ++ show name ++ "not found")
-envLookup (Env dict parentEnv) name =
-  dict & Map.lookup name & Maybe.fromMaybe (envLookup parentEnv name)
+envLookup (Env dict _ parentEnv) name =
+  let _ = if name == "x" then debugPipe "### x" value else value
+      value = dict & Map.lookup name & Maybe.fromMaybe (envLookup parentEnv name)
+   in value
 
-envSet :: Env -> String -> Expression -> Env
-envSet (Env dict parent) name value =
+orElse :: Maybe a -> Maybe a -> Maybe a
+orElse a@(Just x) _ = a
+orElse Nothing b@(Just x) = b
+orElse Nothing Nothing = Nothing
+
+envTypeLookup :: Env -> String -> Maybe Expression
+envTypeLookup EmptyEnv name = Nothing
+envTypeLookup (Env _ typeDict parentEnv) name =
+  typeDict & Map.lookup name & orElse (envTypeLookup parentEnv name)
+
+envLookupAndEval :: Env -> String -> (Env, Expression)
+envLookupAndEval EmptyEnv name = (EmptyEnv, anyType)
+envLookupAndEval env@(Env dict _ parentEnv) name =
+  case Map.lookup name dict of
+    Just expr ->
+      let value = valuate env expr
+       in (envSetValue env name value, value & debugPipe "envLookupAndEval")
+    Nothing ->
+      envLookupAndEval parentEnv name
+
+envSetValue :: Env -> String -> Expression -> Env
+envSetValue (Env dict typeDict parent) name value =
   if not (Map.member name dict)
-    then Env (Map.insert name value dict) parent
+    then Env (Map.insert name value dict) typeDict parent
     else error $ "Can't override existing binding for " ++ name
 
-makeChild :: Env -> Env
-makeChild = Env Map.empty
+envSetType :: Env -> String -> Expression -> Env
+envSetType (Env valDict typeDict parent) name typeValue =
+  case Map.lookup name typeDict of
+    Just existingType ->
+      if typeValue `isSubset` existingType == OBool True
+        then Env valDict (Map.insert name typeValue typeDict) parent
+        else error "Types aren't compatible"
+    Nothing -> Env valDict (Map.insert name typeValue typeDict) parent
 
-setParent :: Env -> Env -> Env
-setParent (Env dict EmptyEnv) = Env dict
+makeChild :: Env -> Env
+makeChild = Env Map.empty Map.empty
+
+setRootParent :: Env -> Env -> Env
+setRootParent (Env dict typeDict EmptyEnv) newParent = Env dict typeDict newParent
+setRootParent (Env dict typeDict parentEnv) newParent = Env dict typeDict (setRootParent parentEnv newParent)
 
 defaultEnv :: Env
-defaultEnv = Env Map.empty EmptyEnv
+defaultEnv = Env Map.empty Map.empty EmptyEnv
 
 toBuiltinNum :: Expression -> Int
 toBuiltinNum (ONumber n) = n
@@ -66,7 +99,7 @@ toBuiltinBool (OBool b) = b
 toBuiltinStr :: Expression -> String
 toBuiltinStr (OString s) = s
 
-evalBinaryNum :: Env -> (Int -> Int -> Expression) -> Expression -> Expression -> (Env, Expression)
+evalBinaryNum :: Env -> (Int -> Int -> Expression) -> Expression -> Expression -> Expression
 evalBinaryNum env fn arg1 arg2 =
   let isNumber expr = case expr of
         ONumber _ -> True
@@ -75,48 +108,66 @@ evalBinaryNum env fn arg1 arg2 =
       varg1 = valuate env arg1
       varg2 = valuate env arg2
    in if isNumber varg1 && isNumber varg2
-        then (env, fn (toBuiltinNum varg1) (toBuiltinNum varg2))
-        else (env, Error "Tried to call a built-in function with a non-number argument")
+        then fn (toBuiltinNum varg1) (toBuiltinNum varg2)
+        else Error "Tried to call a built-in function with a non-number argument"
+
+zipFresh :: [a] -> [b] -> ([(a, b)], ([a], [b]))
+zipFresh [] bs = ([], ([], bs))
+zipFresh as [] = ([], (as, []))
+zipFresh (a : as) (b : bs) =
+  let (futurePairs, leftovers) = zipFresh as bs
+   in ((a, b) : futurePairs, leftovers)
 
 -- >>> valuate defaultEnv (Value (ONumber 2))
 --
 eval :: Env -> Expression -> (Env, Expression)
 eval env expr =
-  case expr & debugPipe "Evaluating" of
-    (Name name) -> (env, envLookup env name)
-    (IfElse condExpr trueExpr falseExpr) ->
-      case valuate env condExpr of
-        OBool True -> eval env trueExpr
-        OBool False -> eval env falseExpr
-        _ -> (env, Error $ "conditional expressions must have a boolean argument, got " ++ show (valuate env condExpr) ++ " instead")
-    (Call (Name ">") [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a > b)) arg1 arg2
-    (Call (Name ">=") [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a >= b)) arg1 arg2
-    (Call (Name "<") [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a < b)) arg1 arg2
-    (Call (Name "<=") [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a <= b)) arg1 arg2
-    (Call (Name "=") [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a == b)) arg1 arg2
-    (Call (Name "-") [arg1, arg2]) -> evalBinaryNum env (\a b -> ONumber (a - b)) arg1 arg2
-    (Call (Name "+") [arg1, arg2]) -> evalBinaryNum env (\a b -> ONumber (a + b)) arg1 arg2
-    (Call (Name "*") [arg1, arg2]) -> evalBinaryNum env (\a b -> ONumber (a * b)) arg1 arg2
-    (Call (Lambda [] lenv body) []) -> (env, valuate (setParent lenv env) body)
-    (Call (Lambda [] lenv body) args) -> (env, valuate env (Call (valuate (setParent lenv env) body) args)) & debugPipe "weird call"
-    (Call l@(Lambda {}) []) -> (env, l)
-    (Call (Lambda (n : argNames) lenv body) (a : args)) ->
-      let childEnv = bind (Bind n (valuate env a)) lenv
-       in eval env (Call (Lambda argNames childEnv body) args)
-    (Call (Type _) args) -> (env, Error "Can't call a type")
-    (Call (ONumber _) args) -> (env, Error "Can't call a number")
-    (Call fnExpr args) -> eval env (Call (valuate env fnExpr) args)
-    (Let [] e) -> eval env e
-    (Let (b : bs) e) ->
-      let newEnv = bind b env
-       in eval newEnv (Let bs e)
-    (Error str) -> error str
-    (Lambda names _ args) -> (env, Lambda names env args & debugPipe "lambda raw eval")
-    expr -> (env, expr) -- a lot of values evaluate to themselves
+  let res = case expr & debugPipe "Starting eval" of
+        (Name name) -> envLookupAndEval env name
+        (IfElse condExpr trueExpr falseExpr) ->
+          ( env,
+            case valuate env condExpr of
+              OBool True -> valuate env trueExpr
+              OBool False -> valuate env falseExpr
+              _ -> Error $ "conditional expressions must have a boolean argument, got " ++ show (valuate env condExpr) ++ " instead"
+          )
+        (Call fnExpr args) ->
+          ( env,
+            case (fnExpr, args) of
+              (Name ">", [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a > b)) arg1 arg2
+              (Name ">=", [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a >= b)) arg1 arg2
+              (Name "<", [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a < b)) arg1 arg2
+              (Name "<=", [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a <= b)) arg1 arg2
+              (Name "=", [arg1, arg2]) -> evalBinaryNum env (\a b -> OBool (a == b)) arg1 arg2
+              (Name "-", [arg1, arg2]) -> evalBinaryNum env (\a b -> ONumber (a - b)) arg1 arg2
+              (Name "+", [arg1, arg2]) -> evalBinaryNum env (\a b -> ONumber (a + b)) arg1 arg2
+              (Name "*", [arg1, arg2]) -> evalBinaryNum env (\a b -> ONumber (a * b)) arg1 arg2
+              (Name "inferType", [Quote x]) -> inferType env x
+              (Lambda [] lenv body, []) -> valuate (setRootParent (lenv & debugPipe "lenv") env & debugPipe "lenvRooted") (body & debugPipe "evaluating body")
+              (Lambda [] lenv body, args) -> valuate env (Call (valuate (setRootParent lenv env) body) args) & debugPipe "weird call"
+              (l@(Lambda {}), []) -> l & debugPipe "evaluating lambda, ran out of args"
+              (Lambda (n : argNames) lenv body, a : args) ->
+                valuate env (Call (Lambda argNames lenv (Let [BindValue n (valuate env a)] body)) args)
+              (Type _, args) -> Error "Can't call a type"
+              (ONumber _, args) -> Error "Can't call a number"
+              _ -> valuate env (Call (valuate env fnExpr) args)
+          )
+        (Let [] e) -> eval env e
+        (Let bs e) ->
+          let childEnv = makeChild env
+              newEnv = List.foldl (flip bind) childEnv bs
+           in eval newEnv e
+        (Error str) -> error str
+        (Lambda names EmptyEnv args) -> (env, Lambda names env args & debugPipe "lambda raw eval")
+        expr -> (env, expr) -- a lot of values evaluate to themselves
+      (resEnv, value) = res
+   in (resEnv, value & debugPipe (" = eval " ++ show expr))
 
 bind :: Binding -> Env -> Env
-bind (Bind name expr) env =
-  envSet env name expr
+bind (BindValue name expr) env =
+  envSetValue env name expr
+bind (BindType name expr) env =
+  envSetType env name expr
 
 valuate :: Env -> Expression -> Expression
 valuate env expr = value
@@ -132,54 +183,94 @@ boolType = Type (OString "Bool")
 typeType :: Expression
 typeType = Type (OString "Type")
 
+anyType :: Expression
+anyType = Type (OString "Any")
+
+isSubset :: Expression -> Expression -> Expression
+(Type t) `isSubset` (Type (OString "Any")) = OBool True
+(TypeFun _ _) `isSubset` (Type (OString "Any")) = OBool True
+(Type t) `isSubset` (Type u) =
+  if u == typeType || u == anyType
+    then OBool True
+    else OBool ((t == u) & debugPipe " !!!! Manual type checking")
+t `isSubset` u = Error "Can't check if nontypes are subsets"
+
+isInstanceOf :: Env -> Expression -> Expression -> Expression
+isInstanceOf env expr typ@(Type (OString "Any")) = OBool True
+isInstanceOf env expr typ@(Type _) = inferType env expr `isSubset` typ
+isInstanceOf _ _ _ = Error "Can't check if value is subset of non-type"
+
+-- | >>> callReduction (TypeFun numType numType) [numType] == numType
+callReduction :: Env -> Expression -> [Expression] -> Expression
+callReduction _ funType [] = funType
+callReduction env (TypeFun t ts) (a : rgs) =
+  if isInstanceOf env a t == OBool True
+    then callReduction env ts rgs -- TODO argument binding
+    else Error "Infertype: wrong argument type"
+callReduction _ _ _ = Error "Error during type inference of a function call"
+
+inferPass :: Env -> Expression -> Expression
+inferPass env expression = Error "noType"
+
+checkType :: Env -> Expression -> Expression -> (Env, Expression)
+checkType env expr typ = (env, OBool True)
+
+toFunType :: [Expression] -> Expression
+toFunType = foldr TypeFun anyType
+
 inferType :: Env -> Expression -> Expression
-inferType _ (ONumber _) = numType
-inferType _ (OString _) = Type (OString "String")
-inferType _ (OBool _) = boolType
-inferType _ (Type _) = typeType
-inferType _ (TypeFun {}) = typeType
-inferType _ (TypeOr {}) = typeType
-inferType _ (Name "-") = TypeFun numType (TypeFun numType numType)
-inferType _ (Name "+") = TypeFun numType (TypeFun numType numType)
-inferType _ (Name "<=") = TypeFun numType (TypeFun numType boolType)
-inferType _ (Name ">=") = TypeFun numType (TypeFun numType boolType)
-inferType _ (Name "=") = TypeFun numType (TypeFun numType boolType)
-inferType env (Name n) = envLookup env n & inferType env
-inferType env (Let [] x) = inferType env x
-inferType env (Let (b : bs) x) = inferType (bind b env) (Let bs x)
-inferType env (Lambda [] lenv body) =
-  inferType env body
-inferType env (Lambda (n : ns) lenv body) =
-  TypeFun numType (inferType env (Lambda ns lenv body))
-inferType env (Call fun args) =
-  let funType = inferType env fun
-
-      callReduction :: Expression -> [Expression] -> Expression
-      callReduction funType [] = funType
-      callReduction (TypeFun t ts) (a : rgs) = callReduction ts rgs -- TODO if `inferType a == t`
-      callReduction _ args = Error "Error during type inference of a function call"
-   in callReduction funType args
-inferType env (IfElse condExpr trueExpr falseExpr) =
-  let trueType = inferType env trueExpr
-      falseType = inferType env falseExpr
-   in if trueType == falseType
-        then trueType
-        else IfElse condExpr trueType falseType
-
--- _ -> Error "conditional expressions must have a boolean argument"
+inferType env expr =
+  case expr & debugPipe "inferType" of
+    ONumber _ -> numType
+    OString _ -> Type (OString "String")
+    OBool _ -> boolType
+    Type _ -> typeType
+    TypeFun {} -> typeType
+    TypeOr {} -> typeType
+    Name "-" -> TypeFun numType (TypeFun numType numType)
+    Name "+" -> TypeFun numType (TypeFun numType numType)
+    Name "<=" -> TypeFun numType (TypeFun numType boolType)
+    Name ">=" -> TypeFun numType (TypeFun numType boolType)
+    Name "=" -> TypeFun numType (TypeFun numType boolType)
+    Name "inferType" -> TypeFun (Type (OString "Quote")) typeType
+    Name n -> case envTypeLookup env n of
+      Just t -> t
+      Nothing -> envLookup env n & inferType env
+    Let bs x ->
+      valuate env (Let bs (Call (Name "inferType") [Quote x]))
+    Lambda [] lenv body ->
+      inferType env body
+    Lambda (n : ns) lenv body ->
+      TypeFun anyType (inferType env (Lambda ns lenv body))
+    Call (Name n) args ->
+      let existingFunType = envTypeLookup env n & Maybe.fromMaybe anyType
+          argTypes = List.map (inferType env) args
+          requiredType = toFunType argTypes
+       in if (requiredType `isSubset` existingFunType) == OBool True
+            then callReduction env requiredType args
+            else error $ "Call type " ++ show requiredType ++ " not compatible with existing definion " ++ show existingFunType
+    Call fun args ->
+      let funType = inferType env fun
+       in callReduction env funType args
+    IfElse condExpr trueExpr falseExpr ->
+      -- TODO check typeof condExpr is bool
+      let trueType = inferType env trueExpr
+          falseType = inferType env falseExpr
+       in if trueType == falseType
+            then trueType
+            else IfElse condExpr trueType falseType
 
 main :: IO ()
 main =
-  -- let res = valuate defaultEnv (Call (Name "+") [ONumber 3, ONumber 2])
   let longExpr =
         Let
-          [ Bind "myType" (Type (Name "x")),
-            Bind "add" (Lambda ["x", "y"] defaultEnv (Call (Name "+") [Name "x", Name "y"])),
-            Bind
+          [ BindValue "myType" (Type (Name "x")),
+            BindValue "add" (Lambda ["x", "y"] EmptyEnv (Call (Name "+") [Name "x", Name "y"])),
+            BindValue
               "fib"
               ( Lambda
                   ["x"]
-                  defaultEnv
+                  EmptyEnv
                   ( IfElse
                       (Call (Name "<=") [Name "x", ONumber 1])
                       (ONumber 1)
@@ -191,18 +282,28 @@ main =
                       )
                   )
               ),
-            Bind "x" (ONumber 7),
-            Bind "y" (ONumber 3)
+            BindValue "y" (ONumber 3),
+            BindValue "a" (Call (Name "+") [Name "y", ONumber 4])
           ]
           -- (Name "myType")
           -- (Name "x")
-          -- (Name "y")
+          -- (Name "a")
           -- (Name "fib")
-          -- (Call (Name "add") [ONumber 2])
-          (Call (Call (Name "add") [ONumber 2]) [ONumber 3])
-      -- (Call (Name "fib") [Name "x"])
+          -- (Name "add")
+          (Call (Name "add") [ONumber 2])
+      -- (Call (Call (Name "add") [ONumber 2]) [ONumber 3])
+      -- (Call (Name "inferType") [Quote (Name "fib")])
+      -- (Call (Name "fib") [Name "a"])
 
       x = 1
    in do
+        putStrLn ""
+        putStrLn ("> " ++ show longExpr)
         print (valuate defaultEnv longExpr)
+
+        putStrLn ""
+        putStrLn "> inferType ..."
         print (inferType defaultEnv longExpr)
+
+        putStrLn ""
+        putStrLn "done"
